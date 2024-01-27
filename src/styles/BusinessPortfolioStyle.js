e99928f69df032be9901bd448a64a3de80bcc3c6
gyeongisung <gisung.gyeon@gmail.com>
2024-01-27 14:51:33 +0900

    feat: 모바일 반응형 작업

--- src/styles/BusinessPortfolioStyle.js
+++ src/styles/BusinessPortfolioStyle.js
@@ -11,6 +11,15 @@ export const BusinessPortfolioWrap = styled.div`
     font-weight: 700;
     margin-top: 20px;
   }
+  @media all and (max-width: 767px) {
+    width: 360px;
+    height: 150px;
+    & > h2 {
+      color: ${Maincolor.black};
+      font-size: 16px;
+      font-weight: 700;
+    }
+  }
 `;
 
 // 검색바 스타일
@@ -19,7 +28,7 @@ export const PfSearchWrap = styled.div`
   justify-content: space-between;
   align-items: center;
   position: relative;
-  width: 1400px;
+  width: 100%;
   height: 100px;
   border-radius: 10px;
   background: ${Maincolor.search};
@@ -78,6 +87,94 @@ export const PfSearchWrap = styled.div`
       }
     }
   }
+  @media all and (max-width: 767px) {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: 100%;
+    height: 150px;
+    background: ${Maincolor.search};
+    margin-top: 20px;
+    .student-portfolio-search {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: flex-start;
+      padding: 10px 20px;
+      gap: 10px;
+      margin: 10px 0;
+      height: 150px;
+      > li {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        > label {
+          margin-right: 39px;
+        }
+        .subjectname-form {
+          > label {
+            margin-right: 28px;
+          }
+        }
+        .studentname-form {
+          > label {
+            margin-right: 5px;
+          }
+        }
+      }
+      label {
+        font-size: 12px;
+        font-weight: 500;
+        line-height: 14px;
+        letter-spacing: -0.4px;
+        margin-right: 0;
+        margin-left: 0;
+      }
+      & li > select {
+        border: 1px solid ${Maincolor.input};
+        width: 250px;
+        height: 25px;
+        padding: 8px 12px;
+        align-items: center;
+        gap: 10px;
+      }
+      & li > div > input {
+        width: 250px;
+        height: 25px;
+        border: 1px solid ${Maincolor.input};
+        border-radius: 4px;
+        padding: 8px 10px;
+      }
+      & li > button {
+        font-size: 10px;
+        margin-left: 270px;
+        width: 50px;
+        height: 25px;
+        border: none;
+        border-radius: 4px;
+      }
+      /* select창 화살표 커스텀 */
+      .select-wrap {
+        background: linear-gradient(
+          to right,
+          ${Maincolor.search} 20%,
+          ${Maincolor.white} 20%
+        );
+        select {
+          -o-appearance: none;
+          -webkit-appearance: none;
+          -moz-appearance: none;
+          appearance: none;
+          background: url(${process.env.PUBLIC_URL}/assets/Arrowdown.png)
+            no-repeat calc(100% - 12px) 50%/-1px auto;
+          padding: 0 28px 0 10px;
+        }
+        select::-ms-expand {
+          display: none;
+        }
+      }
+    }
+  }
 `;
 
 export const ContentListWrap = styled.div`
@@ -100,6 +197,30 @@ export const ContentListWrap = styled.div`
         height: 24px;
         cursor: pointer;
         margin-left: 20px;
+      }
+    }
+  }
+  @media all and (max-width: 767px) {
+    width: 360px;
+    margin: 0 auto;
+    .content-top-line {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 14px 0;
+      border-bottom: 1px solid ${Maincolor.maingray};
+      margin-top: 30px;
+      & > li {
+        color: ${Maincolor.black};
+        text-align: center;
+        font-size: 14px;
+        font-weight: 400;
+        & svg {
+          width: 24px;
+          height: 24px;
+          cursor: pointer;
+          margin-left: 20px;
+        }
       }
     }
   }
@@ -225,6 +346,94 @@ export const ContentListViewer = styled.div`
         margin-left: 167px;
         & button {
           width: 220px;
+        }
+      }
+    }
+  }
+  @media all and (max-width: 767px) {
+    margin: 0 auto;
+    padding: 10px 0;
+    width: 360px;
+    overflow: auto;
+    .boardStyle {
+      display: flex;
+      justify-content: flex-start;
+      gap: 5px;
+      padding: 10px 0;
+      .boardbox {
+        width: 360px;
+        height: 108px;
+        border-radius: 10px;
+        border: 1px solid ${Maincolor.maingray};
+        padding: 5px 10px;
+        display: flex;
+        justify-content: left;
+        align-items: center;
+        .thumb-img {
+          width: 86px;
+          height: 62px;
+          & > img {
+            width: 86px;
+            height: 62px;
+          }
+        }
+        .board-list {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          margin-left: 4px;
+          color: ${Maincolor.black};
+
+          .oneword {
+            font-size: 9px;
+            font-weight: 500;
+          }
+          .student-name {
+            width: 100%;
+            margin-top: 8px;
+            font-size: 8px;
+            font-weight: 500;
+            letter-spacing: -0.24px;
+          }
+          .subject-name {
+            width: 150px;
+            margin-top: 10px;
+            font-size: 8px;
+            font-weight: 400;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            & > span:first-of-type {
+              display: inline-block;
+              width: 34px;
+              font-weight: 500;
+              margin-right: 1px;
+            }
+          }
+          .date {
+            width: 150px;
+            margin-top: 5px;
+            font-size: 8px;
+            font-weight: 400;
+            & > span:first-of-type {
+              display: inline-block;
+              width: 34px;
+              font-weight: 500;
+              margin-right: 1px;
+            }
+          }
+        }
+        .detail-view-btn {
+          width: 76px;
+          margin-left: 7px;
+          & button {
+            font-size: 6px;
+            width: 76px;
+            height: 18px;
+            border: none;
+            border-radius: 2px;
+            background: ${Maincolor.admintxt};
+          }
         }
       }
     }
--- src/styles/LoginStyle.js
+++ src/styles/LoginStyle.js
@@ -3,10 +3,15 @@ import styled from "@emotion/styled";
 export const LoginWrap = styled.div`
   width: 100%;
   height: 100%;
+  @media all and (max-width: 767px) {
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 export const LoginInner = styled.ul`
   width: 100%;
+  height: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -58,6 +63,75 @@ export const LoginInner = styled.ul`
             border: none;
             border-radius: 6px;
             cursor: pointer;
+          }
+        }
+      }
+    }
+  }
+  @media all and (max-width: 767px) {
+    width: 100%;
+    height: 100%;
+    padding-top: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    li {
+      :first-of-type {
+        display: none;
+        img {
+          display: none;
+        }
+      }
+      :last-of-type {
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        flex-direction: column;
+        gap: 70px;
+        margin-right: 0;
+        .login-title {
+          padding-left: 0;
+          img {
+            width: 235px;
+            height: 20px;
+          }
+        }
+        form {
+          display: flex;
+          justify-content: flex-end;
+          align-items: center;
+          flex-direction: column;
+          gap: 25px;
+          div {
+            width: 100%;
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            gap: 20px;
+            font-size: 16px;
+            font-weight: 600;
+            label {
+              display: none;
+            }
+            input {
+              width: 250px;
+              height: 35px;
+              border: 1px solid #aaa;
+              border-radius: 4px;
+              padding: 0 5px;
+            }
+            button {
+              width: 250px;
+              height: 40px;
+              font-size: 14px;
+              font-weight: 600;
+              color: #fff;
+              background: #228fcf;
+              border: none;
+              border-radius: 6px;
+              cursor: pointer;
+            }
           }
         }
       }
