200b31265711afd06a5936eaa961cde7b7da09ae
gyeongisung <gisung.gyeon@gmail.com>
2024-02-03 00:50:12 +0900

    fix: 쿠키 옵션 수정

--- src/api/cookie.js
+++ src/api/cookie.js
@@ -3,7 +3,13 @@ import { Cookies } from "react-cookie";
 const cookies = new Cookies();
 
 export const setCookie = (name, value) => {
-  return cookies.set(name, value, { path: "/" });
+  return cookies.set(name, value, {
+    path: "/",
+    secure: true,
+    sameSite: "none",
+    httpOnly: false,
+    maxAge: 180,
+  });
 };
 
 export const getCookie = name => {
